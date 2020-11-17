8bd60444fb3a359fc2299a0b4ca2888d1fde8875
wDove1 <69383952+wDove1@users.noreply.github.com>
2020-11-17 16:36:38 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(name='MotorControlPython',
+      version='0.00',
+      url='',
+      license='',
+      author='William Dove',
+      author_email='',
+      description='',
+      packages=,
+      long_description=open('README.md').read(),
+      zip_safe=False)
