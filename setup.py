0ca806a5f3f4865720481ba5ff019d34ff4cf0d7
wDove1 <69383952+wDove1@users.noreply.github.com>
2020-11-20 18:05:02 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='MotorControl',
-    version='0.0.0',
+    version='0.0.4',
     license='Apache License 2.0',
     author='William Dove',
     author_email='williamtdove@gmail.com',
@@ -10,7 +10,7 @@ setup(
     long_description_content_type="text/markdown",
     url='https://github.com/wDove1/Motor-Control-Python',
     packages=find_packages(include=['MotorControl']),
-    install_requires=['RPi.GPIO'],
+    #install_requires=['RPi.GPIO'],
     classifiers=["Programming language :: Python :: 3",
                  "License :: OSI Approved :: Apache License 2.0",
                  "Operating System :: OS Independent"],
